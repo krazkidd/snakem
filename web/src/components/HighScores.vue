a796af36b3360a55a6ab08bb3a285eb834cafcde
Mark Ross <krazkidd@gmail.com>
2023-06-13 19:21:53 -0700

    Make composable function to fetch data.

--- web/src/components/HighScores.vue
+++ web/src/components/HighScores.vue
@@ -1,65 +1,21 @@
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { useFetch } from '../composables/fetch.js'
 
-  const data = ref(null);
-  const loading = ref(true);
-  const error = ref(null);
-
-  function fetchData() {
-    loading.value = true;
-
-    return fetch('/api/highscores', {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    .then(res => {
-      if (!res.ok) {
-        const error = new Error(res.statusText);
-        error.json = res.json();
-
-        throw error;
-      }
-
-      return res.json();
-    })
-    .then(json => {
-      data.value = json;
-    })
-    .catch(err => {
-      error.value = err;
-
-      if (err.json) {
-        return err.json.then(json => {
-          error.value.message = json;
-        });
-      }
-    })
-    .then(() => {
-      loading.value = false;
-    });
-  }
-
-  onMounted(() => {
-    fetchData();
-  });
+  const { data, error } = useFetch('/api/highscores');
 </script>
 
 <template>
-  <div>High Scores</div>
+  <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+  <div v-else-if="data">
+    <div>High Scores</div>
 
-  <ol v-if="!loading && data && data.length">
-    <li v-for="score of data">
-      <strong>{{ score }}</strong>
-    </li>
-  </ol>
-
-  <p v-if="loading">
-   Loading...
-  </p>
-  <p v-if="error">
-  </p>
+    <ol>
+      <li v-for="score of data">
+        <strong>{{ score }}</strong>
+      </li>
+    </ol>
+  </div>
+  <div v-else>Loading...</div>
 </template>
 
 <style>
--- web/src/components/Motd.vue
+++ web/src/components/Motd.vue
@@ -1,59 +1,15 @@
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { useFetch } from '../composables/fetch.js'
 
-  const motd = ref(null);
-  const loading = ref(true);
-  const error = ref(null);
-
-  function fetchData() {
-    loading.value = true;
-
-    return fetch('/api/motd', {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    .then(res => {
-      if (!res.ok) {
-        const error = new Error(res.statusText);
-        error.json = res.json();
-
-        throw error;
-      }
-
-      return res.json();
-    })
-    .then(json => {
-      motd.value = json;
-    })
-    .catch(err => {
-      error.value = err;
-
-      if (err.json) {
-        return err.json.then(json => {
-          error.value.message = json;
-        });
-      }
-    })
-    .then(() => {
-      loading.value = false;
-    });
-  }
-
-  onMounted(() => {
-    fetchData();
-  });
+  const { data, error } = useFetch('/api/motd');
 </script>
 
 <template>
-  <h2 class="text-center">{{ motd }}</h2>
-
-  <p v-if="loading">
-   Loading...
-  </p>
-  <p v-if="error">
-  </p>
+  <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+  <div v-else-if="data">
+    <h2 class="text-center">{{ data }}</h2>
+  </div>
+  <div v-else>Loading...</div>
 </template>
 
 <style>
