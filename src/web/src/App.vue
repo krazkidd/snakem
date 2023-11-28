09ef70218a75010e5622b0822bc7be90680d407f
Mark Ross <krazkidd@gmail.com>
2023-11-27 17:25:14 -0700

    Cleanup.

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -6,9 +6,11 @@ import { RouterLink, RouterView } from 'vue-router';
   <header>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark px-2">
       <RouterLink class="navbar-brand" to="/">Snake-M</RouterLink>
+
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
