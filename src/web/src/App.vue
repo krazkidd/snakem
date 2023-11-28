1d8e03a62f3008a6bbf4d79f79667937a756d1f8
Mark Ross <krazkidd@gmail.com>
2023-11-27 17:01:29 -0700

    Rerun project creation command and merge.
    Adds Vue router, Pinia, Typescript, and formatters.

--- src/web/src/App.vue
+++ src/web/src/App.vue
@@ -1,35 +1,26 @@
-<script setup>
-  import Motd from './components/Motd.vue'
-  import HighScores from './components/HighScores.vue'
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Snake-M</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Snake-M</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <RouterLink class="nav-link" to="/">Home</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 
   <main role="main" class="container-fluid mt-2">
-    <div class="row">
-      <div class="col-12 col-sm-4 col-md-6 col-lg-8">
-
-      </div>
-
-      <div class="col">
-        <Motd />
-
-        <HighScores />
-      </div>
-    </div>
+    <RouterView />
   </main>
 </template>
 
--- src/web/vite.config.mts
+++ src/web/vite.config.mts
@@ -6,7 +6,7 @@ import Components from 'unplugin-vue-components/vite'
 import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+export default defineConfig(({ command }) => {
   return {
     define: {
       __SERVER_URL__: JSON.stringify(command === 'serve' ? 'http://localhost:9000' : ''),
