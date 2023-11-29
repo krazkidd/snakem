b9d173e499df7a3318bd1d1ae16998ebac07636e
Mark Ross <krazkidd@gmail.com>
2023-11-29 09:04:22 -0700

    Pull game component out of router navigation/view.

--- src/web/src/views/AboutView.vue
+++ src/web/src/views/AboutView.vue
@@ -1,15 +1,17 @@
 <template>
-  <p>
-    Play snake with your friends!
-  </p>
+  <div class="m-2">
+    <p>
+      Play snake with your friends!
+    </p>
 
-  <h3>
-    Attributions
-  </h3>
+    <h3>
+      Attributions
+    </h3>
 
-  <p>
-    The snake favicon is used under the <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a> license from the <a href="https://github.com/twitter/twemoji">twemoji</a> project.
-  </p>
+    <p>
+      The snake favicon is used under the <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a> license from the <a href="https://github.com/twitter/twemoji">twemoji</a> project.
+    </p>
+  </div>
 </template>
 
 <style>
--- src/web/src/views/HomeView.vue
+++ src/web/src/views/HomeView.vue
@@ -1,33 +1,12 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
-
-  import PhaserGame from '../components/PhaserGame.vue';
   import MessageOfTheDay from '../components/MessageOfTheDay.vue';
   import HighScores from '../components/HighScores.vue';
-
-  const gameStarted = ref(false);
 </script>
 
 <template>
-  <div class="row h-100">
-    <div class="col-12 col-sm-4 col-md-6 col-lg-8 h-100">
-      <PhaserGame :startGame="gameStarted" />
-    </div>
+  <div class="m-2">
+    <MessageOfTheDay />
 
-    <div class="col p-2">
-      <div class="mb-2">
-        <button type="button"
-          :class="{ 'btn-success': !gameStarted, 'btn-outline-danger': gameStarted }"
-          class="btn"
-          @click="gameStarted = !gameStarted"
-        >
-          {{ gameStarted ? 'Stop Game' : 'Start Game' }}
-        </button>
-      </div>
-
-      <MessageOfTheDay />
-
-      <HighScores />
-    </div>
+    <HighScores />
   </div>
 </template>
